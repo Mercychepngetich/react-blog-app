9f9a011f0643424e88bd15453f866ecdaf259f69
Mercychepngetich <138122032+Mercychepngetich@users.noreply.github.com>
2023-08-23 16:57:26 +0300

    Mercy/navbar (#1)
    * navbar updated
    
    * navbar added and styled

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+function Navbar (){
+    return(
+        <div className="navbar-container">
+            <div className="intro"></div>
+        <div className="nav-list">
+            <h4>Home</h4>
+            <h4>Blogs</h4>
+            <h4>Certificates</h4>
+            <h4>Inbox</h4>
+        </div>
+        </div>
+    )
+}
+export default Navbar
